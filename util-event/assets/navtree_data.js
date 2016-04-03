a2c805da9b6bad8af6c9e766303404d04a74961a
Marvin Haschker <marvin@haschker.me>
2016-04-03 15:37:18 +0200

    Add Apidoc for util-event

---
+++ util-event/assets/navtree_data.js
@@ -0,0 +1,9 @@
+var NAVTREE_DATA =
+[ [ "net.gtaun.util.event", "net/gtaun/util/event/package-summary.html", [ [ "Interfaces", null, [ [ "EventHandler", "net/gtaun/util/event/EventHandler.html", null, "" ], [ "EventManager", "net/gtaun/util/event/EventManager.html", null, "" ], [ "EventManagerNode", "net/gtaun/util/event/EventManagerNode.html", null, "" ], [ "HandlerEntry", "net/gtaun/util/event/HandlerEntry.html", null, "" ], [ "Interruptable", "net/gtaun/util/event/Interruptable.html", null, "" ], [ "ThrowableHandler", "net/gtaun/util/event/ThrowableHandler.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "AbstractHandlerEntry", "net/gtaun/util/event/AbstractHandlerEntry.html", null, "" ], [ "Attentions", "net/gtaun/util/event/Attentions.html", null, "" ], [ "Event", "net/gtaun/util/event/Event.html", null, "" ], [ "EventManagerRoot", "net/gtaun/util/event/EventManagerRoot.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "HandlerPriority", "net/gtaun/util/event/HandlerPriority.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ util-event/reference/lists.js
@@ -0,0 +1,15 @@
+var DATA = [
+      { id:0, label:"net.gtaun.util.event", link:"net/gtaun/util/event/package-summary.html", type:"package" },
+      { id:1, label:"net.gtaun.util.event.AbstractHandlerEntry", link:"net/gtaun/util/event/AbstractHandlerEntry.html", type:"class" },
+      { id:2, label:"net.gtaun.util.event.Attentions", link:"net/gtaun/util/event/Attentions.html", type:"class" },
+      { id:3, label:"net.gtaun.util.event.Event", link:"net/gtaun/util/event/Event.html", type:"class" },
+      { id:4, label:"net.gtaun.util.event.EventHandler", link:"net/gtaun/util/event/EventHandler.html", type:"class" },
+      { id:5, label:"net.gtaun.util.event.EventManager", link:"net/gtaun/util/event/EventManager.html", type:"class" },
+      { id:6, label:"net.gtaun.util.event.EventManagerNode", link:"net/gtaun/util/event/EventManagerNode.html", type:"class" },
+      { id:7, label:"net.gtaun.util.event.EventManagerRoot", link:"net/gtaun/util/event/EventManagerRoot.html", type:"class" },
+      { id:8, label:"net.gtaun.util.event.HandlerEntry", link:"net/gtaun/util/event/HandlerEntry.html", type:"class" },
+      { id:9, label:"net.gtaun.util.event.HandlerPriority", link:"net/gtaun/util/event/HandlerPriority.html", type:"class" },
+      { id:10, label:"net.gtaun.util.event.Interruptable", link:"net/gtaun/util/event/Interruptable.html", type:"class" },
+      { id:11, label:"net.gtaun.util.event.ThrowableHandler", link:"net/gtaun/util/event/ThrowableHandler.html", type:"class" }
+
+    ];
